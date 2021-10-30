8a06387c7aa19fb2c77f89bed2428cfd430596ca
aoki <ringohub@gmail.com>
2021-10-30 11:59:44 +0900

    Add state to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,41 @@
-use axum::{handler::get, Router};
+use axum::{extract, handler::get, AddExtensionLayer, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let server = Server::new();
+
+    let app = Router::new()
+        .route(
+            "/",
+            get(|_state: extract::Extension<Server>| async move {
+                format!("Hello, World! {}", _state.name)
+            }),
+        )
+        .route(
+            "/foo",
+            get(
+                |_state: extract::Extension<Server>| async move { format!("/foo {}", _state.name) },
+            ),
+        )
+        .layer(AddExtensionLayer::new(server));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+// サーバーで持ち回る状態
+use serde::{Deserialize, Serialize};
+#[derive(Deserialize, Serialize, Clone)]
+pub struct Server {
+    name: String,
+}
+
+impl Server {
+    pub fn new() -> Self {
+        Server {
+            name: "hello".into(),
+        }
+    }
 }
